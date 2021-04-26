2f51fe208f6f73054a7ab54f0ba7d229c2240eac
miquelpaucasesnoves <miquelfrontera@paucasesnovescifp.cat>
2021-04-26 15:13:55 +0200

    First commit, WPF form design done.
    Also done the object class assembledproduct and DataAccess.

---
+++ BillMaterialsWPF/AssembledProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillMaterialsWPF
+{
+    public class AssembledProduct
+    {
+        public int productAssemblyID;
+
+        public int GetProductAssemnblyID()
+        {
+            return productAssemblyID;
+        }
+
+        private void SetProductAssemblyID(int value)
+        {
+            productAssemblyID = value;
+        }
+
+        public string Name { get; set; }
+        public List<String> Components { get; set; }
+
+    }
+}
---
+++ BillMaterialsWPF/DataAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace BillMaterialsWPF
+{
+    public class DataAccess
+    {
+        static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = AdventureWorks2016; 
+            Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; 
+            ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+
+        //Return all the assembled products of the database
+        public List<AssembledProduct> GetAssembledProducts()
+        {
+            List<AssembledProduct> products;
+            string sql = $"SELECT distinct Production.Product.Name as Name,ProductAssemblyID" +
+                                $" FROM Production.BillOfMaterials " +
+                                $"JOIN Production.Product on Product.ProductID = ProductAssemblyID";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                products = conn.Query<AssembledProduct>(sql).ToList();
+            }
+            return products;
+        }
+
+
+        //Return a component List of a assembled product
+        public List<string> GetComponents(int productAssemblyID)
+        {
+            List<string> components;
+            string sql = $"SELECT Production.Product.Name as Name" +
+                                $" FROM Production.BillOfMaterials " +
+                                $"JOIN Production.Product on Product.ProductID = ComponentID " +
+                                $"WHERE ProductAssemblyID = {productAssemblyID}";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                components = conn.Query<string>(sql).ToList();
+            }
+            return components;
+        }
+    }
+}
